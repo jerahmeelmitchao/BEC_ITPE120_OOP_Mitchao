71f93e89b996993b659e3813fe5f7acfba146903
jerahmeelmitchao <148676381+jerahmeelmitchao@users.noreply.github.com>
2023-10-22 16:06:25 +0800

    Add files via upload

---
+++ super_in_Constructors.java
@@ -0,0 +1,17 @@
+public class super_in_Constructors {
+    String name;
+
+    super_in_Constructors(String name) {
+        this.name = name;
+    }
+}
+
+class Dog extends super_in_Constructors {
+    String breed;
+
+    Dog(String name, String breed) {
+        super(name);
+        this.breed = breed;
+    }
+}
+
